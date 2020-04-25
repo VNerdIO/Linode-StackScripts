b5db5b41ca4ecda0ff32236b6e03497cd8dc2f91
VNerdIO <34196863+VNerdIO@users.noreply.github.com>
2020-04-25 16:35:11 -0400

    a few changes

--- VNerd-Wordpress.sh
+++ VNerd-Wordpress.sh
@@ -13,47 +13,29 @@ adduser $SSUSER --disabled-password --gecos "" && \
 echo "$SSUSER:$SSPASSWORD" | chpasswd
 adduser $SSUSER sudo
 
+# Firewall
+ufw allow ssh
+ufw allow http
+ufw allow https
+ufw enabled
 
 # updates
 apt update -y
 apt upgrade -y
+apt autoremove -y
 
-#SET HOSTNAME	
+#SET HOSTNAME
 hostnamectl set-hostname $HOSTNAME
 echo "127.0.0.1   $HOSTNAME" >> /etc/hosts
 
 #INSTALL APACHE
-apt-get install apache2 -y
-
-# edit apache config
-sed -ie "s/KeepAlive Off/KeepAlive On/g" /etc/apache2/apache2.conf
-
-
-#Create a copy of the default Apache configuration file for your site:
-cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$WEBSITE.conf
-
-# configuration of vhost file
-cat <<END >/etc/apache2/sites-available/$WEBSITE.conf
-<Directory /var/www/html/$WEBSITE/public_html>
-    Require all granted
-</Directory>
-<VirtualHost *:80>
-        ServerName $WEBSITE
-        ServerAlias www.$WEBSITE
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html/$WEBSITE/public_html
-
-        ErrorLog /var/www/html/$WEBSITE/logs/error.log
-        CustomLog /var/www/html/$WEBSITE/logs/access.log combined
-
-</VirtualHost>
-END
+apt-get install nginx -y
 
 # Make public_html & logs
 mkdir -p /var/www/html/$WEBSITE/{public_html,logs,src}
 
 # Remove default apache page
-rm /var/www/html/index.html
+rm /var/www/html/*.html
 
 # Install wordpress
 cd /var/www/html/$WEBSITE/src/
@@ -63,20 +45,6 @@ sudo -u www-data tar -xvf latest.tar.gz
 sudo mv latest.tar.gz wordpress-`date "+%Y-%m-%d"`.tar.gz
 sudo mv wordpress/* ../public_html/
 sudo chown -R www-data:www-data /var/www/html/$WEBSITE/public_html
-
-
-#Link your virtual host file from the sites-available directory to the sites-enabled directory:
-sudo a2ensite $WEBSITE.conf
-
-
-#Disable the default virtual host to minimize security risks:
-a2dissite 000-default.conf
-
-
-# restart apache
-systemctl reload apache2
-systemctl restart apache2
-
 
 # Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
 echo "mysql-server mysql-server/root_password password $DB_PASSWORD" | sudo debconf-set-selections
@@ -91,7 +59,7 @@ mysql -uroot -p$DB_PASSWORD -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DBUSER'
 service mysql restart
  
 #installing php
-apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql -y php-gd
+apt install php php-pear libapache2-mod-php7.0 php-mysql php-gd -y
 
 # making directory for php? giving apache permissions to that log? restarting php
 mkdir /var/log/php
